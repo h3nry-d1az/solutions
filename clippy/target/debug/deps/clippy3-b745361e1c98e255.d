9bc91d71efb951db1ca27a8baef0494addb2dd2d
Henry Díaz Bordón <henrydiazbordon@gmail.com>
2022-11-26 14:14:54 +0100

    added the solutions

---
+++ clippy/target/debug/deps/clippy3-b745361e1c98e255.d
@@ -0,0 +1,7 @@
+C:\Users\henry\rustlings\exercises\clippy\target\debug\deps\clippy3-b745361e1c98e255.rmeta: clippy3.rs
+
+C:\Users\henry\rustlings\exercises\clippy\target\debug\deps\clippy3-b745361e1c98e255.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
